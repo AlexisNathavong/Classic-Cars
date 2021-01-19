bcf41a123f9c36faa5494f27a6a0159171e50fc2
Diana Grams <deegrams221@gmail.com>
2021-01-19 12:50:47 -0700

    Footer basic setup

---
+++ classic-cars/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <div className='footer'>
+        {/* .....Classic Cars Logo..... */}
+        <div className='footer-copyright'>
+          <p>Classic Cars© All rights reserved.</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;

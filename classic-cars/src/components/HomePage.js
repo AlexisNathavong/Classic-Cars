6d0ddb0c871031eba9dd4c5d1f3d85cfa06ec337
Diana Grams <deegrams221@gmail.com>
2021-01-19 10:42:13 -0700

    Home Page setup

---
+++ classic-cars/src/components/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <>
+      <div className='home-page'>
+        <div className='home-header'>
+          <h1>Classic Cars</h1>
+        </div>
+        <div className='car-info'>
+          {/* display car info with images from database as cards here */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
